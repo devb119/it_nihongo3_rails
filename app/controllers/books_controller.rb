9d6d82191b67aeb5ce6b5283979094dd4f682fd6
devb119 <ducanh11092001@gmail.com>
2023-01-10 17:51:57 +0000

    add image on book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -66,6 +66,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number_of_pages)
+      params.require(:book).permit(:title, :number_of_pages, :image)
     end
 end
