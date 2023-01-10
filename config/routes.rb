2d67143141886a8a39bc63cca0c1d8eea88e6f14
devb119 <ducanh11092001@gmail.com>
2023-01-10 10:21:33 +0000

    not finished yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   devise_for :users
   resources :books
+  
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
