eb157d930fce182c7a2fc69be63ed406fe07c7d1
devb119 <ducanh11092001@gmail.com>
2023-01-16 10:18:29 +0000

    Add avatar on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
  protected
 
  def configure_permitted_parameters
-    attributes = [:name, :age, :email]
+    attributes = [:name, :age, :avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-#   validates :name, presence: true
-#   validates :age, presence: true
+  has_one_attached :avatar
 end
